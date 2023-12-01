10a5ec999b5cb97f573ef06ad77e649c560ddf1b
josevidal1601 <josevidal_1601@hotmail.com>
2023-12-01 06:08:03 +0000

    Create analyze and generate image IA functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,132 @@
 import React from 'react';
+import { useState } from 'react';
+import './App.css';
+import analyzeImage, { isConfigured } from './azure-image-analysis';
+import generateImage, { isConfiguredOpenIa } from './openAI-image-generation';
+// import react-dom
+import ReactDOM from 'react-dom';
 
+// Create function app to render a title and form input with two buttons
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+  const [inputAnalyze, setInputAnalyze] = useState('');
+  const resultDiv = document.getElementById('result');
+  let titleResponse = '';
+
+  const isConfiguredImageAnalysis = isConfigured();
+  const isConfiguredImageGeneration = isConfiguredOpenIa();
+
+  if (!isConfiguredImageAnalysis || !isConfiguredImageGeneration) {
+    return (
+      <div className="App">
+        <p>Key and/or endpoint not configured for cognitive services.</p>
+      </div>
+    );
+  }
+
+  function LoadingPage(props) {
+    if (props.loading) {
+      return <p>Loading...</p>;
+    }
+  }
+
+  function AnalyzeClick() {
+    console.log('Click happened');
+    // render loadingPage function
+    ReactDOM.render(
+      LoadingPage({loading: true}),
+      resultDiv
+    );
+
+    // call function analyzeImage and print response in console
+    analyzeImage(inputAnalyze).then(response => {
+      // convert response to json
+      // console.log(JSON.stringify(response));
+      titleResponse = "Computer Vision Analysis";
+      ReactDOM.render(
+        DisplayResults({response: response, url: inputAnalyze}),
+        resultDiv
+      );
+    }, error => {
+      console.error(error);
+      ReactDOM.render(
+        <>
+          <p>Ocurrió un error inesperado: {error.message}</p>
+          <pre>{JSON.stringify(error, null, 2)}</pre>
+        </>,
+        resultDiv
+      );
+    });
+  }
+
+  function GenerateClick() {
+    console.log('Click happened');
+    // render loadingPage function
+    ReactDOM.render(
+      LoadingPage({loading: true}),
+      resultDiv
+    );
+
+    // call function generateImage and print response in console
+    generateImage(inputAnalyze).then(response => {
+      // convert response to json
+      // console.log(JSON.stringify(response));
+      titleResponse = "Image Generation";
+      ReactDOM.render(
+        DisplayResults({response: response, url: response.data[0].url}),
+        resultDiv
+      );
+    }, error => {
+      console.error(error);
+      ReactDOM.render(
+        <>
+          <p>Ocurrió un error inesperado: {error.message}</p>
+          <pre>{JSON.stringify(error, null, 2)}</pre>
+        </>,
+        resultDiv
+      );
+    });
+  }
+
+  function DisplayResults(props) {
+    if (props.response) {
+      return (
+        <>
+          <hr/>
+          <h2>{titleResponse}</h2>
+          <img src={props.url} alt="pic analyzed" width="400" height="auto" />
+          <p>URL: {props.url}</p>
+          <pre
+          style={{
+            overflowX: "auto",
+            whiteSpace: "pre-wrap",
+            wordWrap: "break-word",
+          }}
+          >
+            {JSON.stringify(props.response, null, 2)}
+          </pre>
+        </>
+      );
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Computer vision</h1>
+      <label>Insert URL or type prompt: </label>
+      <br></br>
+      <input name="inputAnalyze" id="inputAnalyze" value={inputAnalyze} 
+        type="text" placeholder="Enter URL to analyze or textual prompt to generate an image" 
+        onChange={e => setInputAnalyze(e.target.value)}/>
+      <br></br>
+      <button className="btnAnalyze" onClick={AnalyzeClick}>Analyze</button>
+      <button className="btnGenerate" onClick={GenerateClick}>Generate</button>
+      <div id="result"></div>
+    </div>
+  );
+
+};
+
+
+
 
 export default App;
---
+++ src/openAI-image-generation.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const generateImage = async (text) => {    
+    const openAiKey = process.env.REACT_APP_OPENAI_API_KEY;
+    const urlBase = "https://api.openai.com/v1/images/generations";
+    const bearerToken = "Bearer " + openAiKey;
+    const requestData = {
+        model: "dall-e-2",
+        prompt: text,
+        n: 1,
+        size: "512x512", // Set the desired image size here
+        style: "natural",
+    };
+    
+    const headers = {
+      'Content-Type': 'application/json',
+       Authorization: bearerToken,
+    };
+      
+    const response = await axios.post(urlBase, requestData, {
+        headers: headers,
+    });
+    return response.data;
+}
+
+function isConfiguredOpenIa() {
+    return process.env.REACT_APP_OPENAI_API_KEY;
+}
+
+export { isConfiguredOpenIa };
+export default generateImage;
